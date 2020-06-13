7bea9fb94507084b5b42f7626c6abbc59679ddb3
Fabiana-Soares <fsf.fabiana8@gmail.com>
2020-06-13 09:52:10 -0300

    update all files

---
+++ Frontend/build/precache-manifest.2e3d21f1822eedb842e745e83e937a4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d979ec49b3dc443289b0f856431c656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18a2453dda86adc97ab0",
+    "url": "/static/css/main.bbc23218.chunk.css"
+  },
+  {
+    "revision": "7b5524f4a7b964540d7c",
+    "url": "/static/js/2.67ead88b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67ead88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a2453dda86adc97ab0",
+    "url": "/static/js/main.260db051.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
